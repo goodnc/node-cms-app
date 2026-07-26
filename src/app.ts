447895d2a3b97814e69fe4d23e670e39bb117a6e
JackNiu <goodnc@163.com>
2026-07-26 11:19:21 +0800

    Add routers into app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import express, { Application } from "express";
 
+import home from "./controllers/home-controller";
+import article from "./controllers/article-controller";
+import user from "./controllers/user-controller";
+import login from "./controllers/login-controller";
+
 const app: Application = express();
+
+app.use("/", home);
+app.use("/article", article);
+app.use("/user", user);
+app.use("/login", login);
 
 app.listen(8080, () => {
   console.log("网站服务器启动成功，端口：8080，请访问：http://localhost:8080");
